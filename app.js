3755327aa07872c55b780db125291159706e544e
adelaide-chen <ada.chen@berkeley.edu>
2021-01-24 21:35:01 -0800

    testing out update/PUT

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-// npm install --save express body-parser mysql nodemon
+// For installation: npm install --save express body-parser mysql nodemon
+
+/* Database schema?
+    Since the only thing on the top-most level of the JSON object is "article", there didn't seem to be much point keeping it.
+    So when inserting the entry in the database, I decided to remove the top level.
+
+    lead_art and authors are nested JSON objects, using stringify and parse to read and write
+
+    CREATE TABLE stories (
+        id VARCHAR(255), 
+        slug VARCHAR(255), 
+        title VARCHAR(255), 
+        dek VARCHAR(255), 
+        published_date VARCHAR(255), 
+        canonical_url VARCHAR(255), 
+        word_count INT, 
+        tags VARCHAR(255), 
+        embeds VARCHAR(255), 
+        lead_art JSON, 
+        authors JSON
+    )
+*/
+
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -47,14 +69,27 @@ app.post('/', (req, res, next) => {
     const json = req.body.article;
     const lead_art = JSON.stringify(json.lead_art)
     const authors = JSON.stringify(json.authors)
+    // connection.query(
+    //     "INSERT INTO stories (id,slug,title,dek,published_date,canonical_url,word_count,tags,embeds,lead_art,authors) VALUES('"+json.id+"', '"+json.slug+"', '"+json.title+"', '"+json.dek+"', '"+json.published_date+"', '"+json.canonical_url+"', '"+json.word_count+"', '"+json.tags+"', '"+json.embeds+"', '"+lead_art+"', '"+authors+"');"
+    //     , (error, results, fields) => {
+    //     if (error) {
+    //         console.log(error)
+    //         next();
+    //     } else {
+    //         res.end(JSON.stringify(results));
+    //     }
+    // })
+    const update = "UPDATE stories SET slug =?, title=?, dek=?, published_date=?, word_count=?, tags=?, embeds=?, lead_art=?, authors=? WHERE (id=? AND canonical_url=?)";
     connection.query(
-        "INSERT INTO stories (id,slug,title,dek,published_date,canonical_url,word_count,tags,embeds,lead_art,authors) VALUES('"+json.id+"', '"+json.slug+"', '"+json.title+"', '"+json.dek+"', '"+json.published_date+"', '"+json.canonical_url+"', '"+json.word_count+"', '"+json.tags+"', '"+json.embeds+"', '"+lead_art+"', '"+authors+"');"
-        , (error, results, fields) => {
-        if (error) {
-            console.log(error)
-            next();
-        } else {
-            res.end(JSON.stringify(results));
+        update,
+        [json.slug, json.title, json.dek, json.published_date, json.word_count, json.tags, json.embeds, lead_art, authors, json.id, json.canonical_url],
+        (error, results, fields) => {
+            if (error) {
+                console.log(error);
+                next();
+            } else {
+                res.end(JSON.stringify(results));
+            }
         }
-    })
+    )
 })
